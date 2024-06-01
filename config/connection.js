5effdb66a376bb8bc2b335e27f9f764fb1437ef8
gocrushgoals <gocrushgoals@gmail.com>
2024-05-31 23:19:03 -0400

    update routes add env

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/socialNetworkDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetwork', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
-const { User, Thought } = require('../models');
+const User = require('../models/User');
+const Thought = require('../models/Thought');
 
-module.exports = {
+
     getUsers(req, res) {
         User.find()
             .then((users) => res.json(users))
@@ -72,4 +73,5 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
-};
+
+module.exports = userRouter;
